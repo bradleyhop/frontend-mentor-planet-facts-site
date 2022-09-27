5ec6c4680d4288e6ca969744ffcdbc496eb47504
bradleyhop <bradleyhop@gmail.com>
2022-09-27 18:21:26 -0400

    switch to component rendering from router; props

--- src/components/PlanetDisplay.vue
+++ src/components/PlanetDisplay.vue
@@ -7,25 +7,31 @@ export default {
       surfaceCopy: false,
     };
   },
+
+  props: {
+    planet: Object,
+  },
 };
 </script>
 
 <template>
-  <h1 class="page-title header-1">Earth</h1>
+  <h1 class="page-title header-1">{{ planet.name }}</h1>
 
   <!-- Overview Content Start -->
 
   <div class="overview-container">
     <p class="overview para-p">
-      Third planet from the Sun and the only known planet to harbor life. About
-      29.2% of Earth's surface is land with remaining 70.8% is covered with
-      water. Earth's distance from the Sun, physical properties and geological
-      history have allowed life to evolve and thrive.
+      {{ planet.overview.content }}
     </p>
 
     <div class="source">
-      Source : <a href="">Wikipedia</a>
-      <!-- URL: https://en.wikipedia.org/wiki/Earth -->
+      Source :
+      <a
+        :href="planet.overview.source"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Wikipedia</a
+      >
     </div>
   </div>
 
@@ -35,15 +41,17 @@ export default {
 
   <div class="internal-container">
     <p class="internal-content para-p">
-      Earth's interior, like that of the other terrestrial planets, is divided
-      into layers by their chemical or physical (rheological) properties. The
-      outer layer is a chemically distinct silicate solid crust, which is
-      underlain by a highly viscous solid mantle.
+      {{ planet.structure.content }}
     </p>
 
     <div class="internal-source">
-      Source : <a href="">Wikipedia</a>
-      <!-- URL: https://en.wikipedia.org/wiki/Earth#Internal_structure -->
+      Source :
+      <a
+        :href="planet.structure.source"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Wikipedia</a
+      >
     </div>
   </div>
 
@@ -53,14 +61,14 @@ export default {
 
   <div class="surface-container">
     <p class="surface-content para-p">
-      The total surface area of Earth is about 510 million km2. The continental
-      crust consists of lower density material such as the igneous rocks granite
-      and andesite. Less common is basalt, a denser volcanic rock that is the
-      primary constituent of the ocean floors.
+      {{ planet.geology.content }}
     </p>
 
     <div class="surface-source">
-      Source : <a href="">Wikipedia</a>
+      Source :
+      <a :href="planet.geology.source" target="_blank" rel="noopener noreferrer"
+        >Wikipedia</a
+      >
       <!-- URL: https://en.wikipedia.org/wiki/Earth#Surface -->
     </div>
   </div>
@@ -92,22 +100,22 @@ export default {
   <div class="number-facts-container">
     <div class="rotation-container">
       <div class="title-fact-number">Rotation Time</div>
-      <div class="number-fact">0.99 Days</div>
+      <div class="number-fact">{{ planet.rotation }}</div>
     </div>
 
     <div class="revolution-container">
       <div class="ti-fact-number">Revolution Time</div>
-      <div class="number-fact">365.26 Days</div>
+      <div class="number-fact">{{ planet.revolution }}</div>
     </div>
 
     <div class="radius-container">
       <div class="title-fact-number">Radius</div>
-      <div class="number-fact">6,371 KM</div>
+      <div class="number-fact">{{ planet.radius }}</div>
     </div>
 
     <div class="temp-container">
       <div class="title-fact-number">Average Temp.</div>
-      <div class="number-fact">16°c</div>
+      <div class="number-fact">{{ planet.temperature }}</div>
     </div>
   </div>
 </template>
